2207ec1c928bbb8f24c66baf791882ecf4bb00e2
Vinit Sharma <vinit123@gmail.com>
2024-05-27 16:22:46 +0530

    Add dynamic environment variable configuration for Elastic Beanstalk

--- .platform/hooks/prebuild/01_set_env.sh
+++ .platform/hooks/prebuild/01_set_env.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "REACT_APP_IMAGE_TAG=build-${TRAVIS_COMMIT}-$(date +'%Y%m%d%H%M%S')" >> /opt/elasticbeanstalk/deployment/env
+
+echo "Setting REACT_APP_IMAGE_TAG environment variable"
+REACT_APP_IMAGE_TAG="build-${TRAVIS_COMMIT}-$(date +'%Y%m%d%H%M%S')"
+echo "REACT_APP_IMAGE_TAG=${REACT_APP_IMAGE_TAG}" >> /opt/elasticbeanstalk/deployment/env
+echo "REACT_APP_IMAGE_TAG=${REACT_APP_IMAGE_TAG}" >> /var/log/eb-hooks.log
